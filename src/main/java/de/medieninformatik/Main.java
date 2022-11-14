801330da4ee21c89955310137260f81ee802e5de
u37424 <lucaspirka@gmail.com>
2022-11-14 08:34:22 +0100

    Better Version

--- src/main/java/de/medieninformatik/Main.java
+++ src/main/java/de/medieninformatik/Main.java
@@ -1,37 +1,46 @@
 package de.medieninformatik;
 
 import java.io.*;
-import java.net.ConnectException;
-import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket("192.168.178.26", 6000)) {
+        try (Socket socket = new Socket("localhost", 6000)) {
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.flush();
 
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            System.out.println("Connected to "+socket.getInetAddress().getHostName());
+            System.out.println("Connected to " + socket.getInetAddress().getHostName());
 
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Username? ");
-            String user = scanner.nextLine();
+
+            Thread read = new Thread(() -> {
+                do {
+                    try {
+                        String s = (String) ois.readObject();
+                        System.out.println(s);
+                    } catch (IOException e) {
+                        System.err.println("Host has given up this conversation.");
+                        break;
+                    } catch (ClassNotFoundException e) {
+                        System.err.println("Host has given up this conversation.");
+                        break;
+                    }
+                } while (true);
+            });
+
+            read.start();
+            String user = InetAddress.getLocalHost().toString();
             do {
                 String text = scanner.nextLine();
-                oos.writeObject(user+": "+text);
-                String received = (String) ois.readObject();
-                System.out.println(received);
+                oos.writeObject(user + ": " + text);
             } while (true);
-        } catch (SocketException | UnknownHostException e){
+        } catch (SocketException | UnknownHostException e) {
             System.err.println("Host has given up this conversation.");
         } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
