44e15751e55fc6ae858ba8b3423c7411cc438a76
u37424 <lucaspirka@gmail.com>
2022-11-14 10:05:36 +0100

    Communicates

--- src/main/java/de/medieninformatik/Main.java
+++ src/main/java/de/medieninformatik/Main.java
@@ -8,38 +8,46 @@ public class Main {
 
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 6000)) {
+        try (Socket socket = new Socket("192.168.43.26", 6000)) {
+            //Streams
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.flush();
 
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
             System.out.println("Connected to " + socket.getInetAddress().getHostName());
 
+            //Eingabe für Client
             Scanner scanner = new Scanner(System.in);
 
+            //Update Text Messages
             Thread read = new Thread(() -> {
                 do {
                     try {
                         String s = (String) ois.readObject();
                         System.out.println(s);
                     } catch (IOException e) {
-                        System.err.println("Host has given up this conversation.");
+                        System.err.println("Host disconnected.");
+                        e.printStackTrace();
                         break;
                     } catch (ClassNotFoundException e) {
-                        System.err.println("Host has given up this conversation.");
+                        System.err.println("Host disconnected.");
+                        e.printStackTrace();
                         break;
                     }
                 } while (true);
             });
-
             read.start();
+
+            //Username des Clients
             String user = InetAddress.getLocalHost().toString();
+
+            //Schreiben
             do {
                 String text = scanner.nextLine();
                 oos.writeObject(user + ": " + text);
             } while (true);
         } catch (SocketException | UnknownHostException e) {
-            System.err.println("Host has given up this conversation.");
+            System.err.println("Host disconnected.");
         } catch (IOException e) {
             e.printStackTrace();
         }
