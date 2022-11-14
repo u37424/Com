e2507ae6f0d4ee5664e765f899c27b505262d743
u37424 <lucaspirka@gmail.com>
2022-11-14 12:41:53 +0100

    Upgrade to FX 2

--- src/main/java/de/medieninformatik/Main.java
+++ src/main/java/de/medieninformatik/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 6000)) {
+        try (Socket socket = new Socket("192.168.43.26", 6000)) {
             //Streams
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.flush();
